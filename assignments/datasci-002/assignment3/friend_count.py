32c758e041ebaae306b3d5c6f9ad8bca5c0cd4e1
Fawad Rashid <fawad@ndataconsulting.com>
2014-07-19 15:44:56 +0500

    added friend count task

---
+++ assignments/datasci-002/assignment3/friend_count.py
@@ -0,0 +1,31 @@
+import MapReduce
+import sys
+
+mr = MapReduce.MapReduce()
+
+# =============================
+# Do not modify above this line
+
+def mapper(record):
+    # key: document identifier
+    # value: document contents
+    
+    key = record[0]
+    value = record[1]
+    
+    mr.emit_intermediate(key, 1)
+	
+def reducer(key, list_of_values):
+    # key: word
+    # value: list of occurrence counts
+    #total = 0
+    #for v in list_of_values:
+    #  total += v
+    friendstotal = len(list_of_values)
+    mr.emit((key, friendstotal))
+
+# Do not modify below this line
+# =============================
+if __name__ == '__main__':
+  inputdata = open(sys.argv[1])
+  mr.execute(inputdata, mapper, reducer)
